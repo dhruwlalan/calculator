eec76efdcd99156ef635be0b531a640c237b0bd9
dhruw <dhruwlalan22@gmail.com>
2020-09-28 23:36:57 +0530

    Finalized Optimizations

--- src/js/components/Preloader.vue
+++ src/js/components/Preloader.vue
@@ -7,26 +7,20 @@
 
 <script>
 	export default {
-		data () {
-			return {
-				loaded: false ,
-				theme: 'light' ,
-			}
-		} ,
+		data () { return {
+			loaded: false ,
+			theme: 'light' ,
+		}},
 		created () {
 			const t = localStorage.getItem('calcTheme');
-            if (t == 'light') {
-            	this.theme = 'light';
-            } else if (t === 'dark') {
-            	this.theme = 'dark'
-            } else {
-            	this.theme = 'light';
-            }
+            if (t == 'light') { this.theme = 'light'; }
+            else if (t === 'dark') { this.theme = 'dark'; }
+            else { this.theme = 'light'; }
 			window.addEventListener('load', () => {
 				setTimeout(() => {
 					this.loaded = true;
 				} , 500);
 			});
-		} ,
+		},
 	}
 </script>
