39de440e01f16b883e359560db87e7c66d1a33db
dhruw <dhruwlalan22@gmail.com>
2021-01-23 19:36:02 +0530

    migrating to vue3 initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="container">
+      <h1>Hello, World!!</h1>
+      <button @click="this.count++">count: {{ count }}</button>
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         count: 0,
+      };
+   },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+///ASSETS///
+import './assets/assets';
+
+///MAIN///
+import { createApp } from 'vue';
+
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
